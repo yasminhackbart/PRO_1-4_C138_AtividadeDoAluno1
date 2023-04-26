7f5b621a375b269b17994b32360a14250f3a6677
Pedro Eduardo Bibiano Pereira <80981386+pedroeduardobibiano@users.noreply.github.com>
2023-04-26 15:37:39 -0300

    Add files via upload

---
+++ train_bot.py
@@ -0,0 +1,61 @@
+#Biblioteca de pré-processamento de dados de texto
+import nltk
+
+from nltk.stem import PorterStemmer
+stemmer = PorterStemmer()
+
+import json
+import pickle
+import numpy as np
+
+words=[]
+classes = []
+word_tags_list = []
+ignore_words = ['?', '!',',','.', "'s", "'m"]
+train_data_file = open('intents.json').read()
+intents = json.loads(train_data_file)
+
+#função para anexar palavras-tronco
+def get_stem_words(words, ignore_words):
+    stem_words = []
+    for word in words:
+        if word not in ignore_words:
+            w = stemmer.stem(word.lower())
+            stem_words.append(w)  
+    return stem_words
+
+for intent in intents['intents']:
+    
+        # Adicione todas as palavras dos padrões à lista
+        for pattern in intent['patterns']:            
+            pattern_word = nltk.word_tokenize(pattern)            
+            words.extend(pattern_word)                      
+            word_tags_list.append((pattern_word, intent['tag']))
+        # Adicione todas as tags à lista classes
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+            stem_words = get_stem_words(words, ignore_words)
+
+print(stem_words)
+print(word_tags_list[0]) 
+print(classes)   
+
+#Crie o corpus de palavras para o chatbot
+def create_bot_corpus(stem_words, classes):
+
+    stem_words = sorted(list(set(stem_words)))
+    classes = sorted(list(set(classes)))
+
+    pickle.dump(stem_words, open('words.pkl','wb'))
+    pickle.dump(classes, open('classes.pkl','wb'))
+
+    return stem_words, classes
+
+stem_words, classes = create_bot_corpus(stem_words,classes)  
+
+print(stem_words)
+print(classes)
+
+# Crie um saco de palavras 
+
+# Crie os dados de treinamento
