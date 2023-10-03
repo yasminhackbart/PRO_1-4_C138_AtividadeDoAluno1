ba4d6cf2533df017921d905ab6ec3f387dab6799
Yasmin Hackbart <90115526+yasminhackbart@users.noreply.github.com>
2023-10-03 20:50:53 -0300

    Update train_bot.py

--- train_bot.py
+++ train_bot.py
@@ -8,11 +8,12 @@ import json
 import pickle
 import numpy as np
 
+training_data=[]
 words=[]
 classes = []
 word_tags_list = []
 ignore_words = ['?', '!',',','.', "'s", "'m"]
-train_data_file = open('intents.json').read()
+train_data_file = open('intents.json', encoding="utf-8").read()
 intents = json.loads(train_data_file)
 
 #função para anexar palavras-tronco
@@ -57,5 +58,42 @@ print(stem_words)
 print(classes)
 
 # Crie um saco de palavras 
+for word_tags in word_tags_list:
+        
+        bag_of_words = []       
+        pattern_words = word_tags[0]
+       
+        for word in pattern_words:
+            index=pattern_words.index(word)
+            word=stemmer.stem(word.lower())
+            pattern_words[index]=word  
+
+        for word in stem_words:
+            if word in pattern_words:
+                bag_of_words.append(1)
+            else:
+                bag_of_words.append(0)
+        print(bag_of_words)
+
+        labels_encoding = list(labels) #inicialmente, labels será toda zerada
+        tag = word_tags[1] #salve a tag
+        tag_index = classes.index(tag)  #vá para o índice da tag
+        labels_encoding[tag_index] = 1  #anexe 1 àquele índice
+       
+        training_data.append([bag_of_words, labels_encoding])
 
 # Crie os dados de treinamento
+def treino(training_data):
+    fraEtiq = np.array(training_data,dtype=object)
+    frase =list(fraEtiq[:,0])
+    etiqueta=list(fraEtiq[:,1])
+    print(frase)
+    print(etiqueta)
+    return frase, etiqueta
+
+frase, etiqueta = treino(training_data)
+
+
+    
+    
+    
